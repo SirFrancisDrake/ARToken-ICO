f7417308001aa73a59fef081c1303135be54877d
SirFrancisDrake <dementia.animi@gmail.com>
2017-08-27 11:12:25 +0300

    Modifying documentation.

--- contracts/GenericCrowdsale.sol
+++ contracts/GenericCrowdsale.sol
@@ -12,7 +12,7 @@ contract GenericCrowdsale {
     /**
      * @dev Confirms a token purchase completed successfully.
      * @param _beneficiary Token holder.
-     * @param _contribution ETH that was sent to the contract.
+     * @param _contribution Ether that was sent to the contract (in wei).
      * @param _tokensIssued The amount of tokens that was assigned to the holder.
      * @param _overcap The amount of Ether (in wei) that went over the cap and was returned.
      */
@@ -20,7 +20,7 @@ contract GenericCrowdsale {
     /**
      * @dev Confirms token issuance for a token purchase that happened off-chain was processed successfully.
      * @param _beneficiary Token holder.
-     * @param _contribution ETH that was sent to the contract.
+     * @param _contribution Ether that was sent to the contract (in wei).
      * @param _txHash Transaction hash from the chain where the money was received.
      * @param _tokensIssued The amount of tokens that was assigned to the holder.
      * @param _overcap The amount of Ether (in wei) that went over the cap and SHOUD BE returned off-chain.
@@ -29,7 +29,17 @@ contract GenericCrowdsale {
     event BonusIssued(address _beneficiary, uint _bonusTokensIssued);
     event FoundersAndPartnersTokensIssued(address foundersWallet, uint tokensForFounders, address partnersWallet, uint tokensForPartners);
 
+    // For contributors on Ethereum
     function buyTokens() public payable returns (bool success);
+
+    /**
+     * @dev Issues tokens for the off-chain contributors by accepting calls from the trusted address. Supposed to be run by the backend.
+     * @param _beneficiary Token holder.
+     * @param _contribution The Ether equivalent (in wei) of the contribution received off-chain.
+     * @param _txHash Transaction hash from the chain where the contribution was received.
+     * @param _tokensIssued The amount of tokens that was assigned to the holder.
+     * @param _overcap The amount of Ether (in wei) that went over the cap and SHOUD BE refunded off-chain by the backend.
+     */
     function offchainBuyTokens(address _beneficiary, uint _contribution) onlyManager external returns (bool success);
     function issueBonus(address _beneficiary) external;
     function rewardFoundersAndPartners() external;
