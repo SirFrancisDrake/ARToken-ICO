b0e985f82aac7b482faee38586a7be502ca403d2
SirFrancisDrake <dementia.animi@gmail.com>
2017-08-31 05:28:15 +0300

    Truffle migration.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,11 @@
-var ARToken = artifacts.require("./ARToken.sol");
+var TokenAllocation = artifacts.require("./TokenAllocation.sol");
+
+var icoManager = "0x0";     // Public key for the backend script that mints tokens
+var foundersWallet = "0x0"; // Public key of Kosta's wallet that will receive tokens after vesting
+var partnersWallet = "0x0"; // Public key of the wallet that allocates early contributors' bonus
+var totalWeiGathered = 0;   // Total sum of all the money gathered throughout the crowdsale
 
 module.exports = function(deployer) {
- deployer.deploy(ARToken, "0x7fb504439b8a99cf1e31dfd0490fd19a7bb502d0");
+ deployer.deploy(TokenAllocation, icoManager, foundersWallet, partnersWallet, totalWeiGathered);
 }; 
 
