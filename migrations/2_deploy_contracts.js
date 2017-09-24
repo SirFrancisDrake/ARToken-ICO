12ae5d8cface6e25b66b297e0fd41c66ea9f425e
SirFrancisDrake <dementia.animi@gmail.com>
2017-09-24 05:46:29 +0300

    Documentation.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,7 +8,7 @@ var totalWeiGathered = 0;   // Total sum of all the money gathered throughout th
 */
 
 // Test data corresponding to ../testrpc.sh, see the keys there
-var icoManager = "0x7fb504439b8a99cf1e31dfd0490fd19a7bb502d0";     // Public key for the backend script that mints tokens
+var icoManager = "0x7fb504439b8a99cf1e31dfd0490fd19a7bb502d0";     // Public key for the manager that launches phases and pauses
 var icoBackend = "0x7fb504439b8a99cf1e31dfd0490fd19a7bb502d0";     // Public key for the backend script that mints tokens
 var foundersWallet = "0xb8d3051d9a97247e592cbc49a1dc14cfa2c0aee0"; // Public key of Kosta's wallet that will receive tokens after vesting
 var partnersWallet = "0xb8d3051d9a97247e592cbc49a1dc14cfa2c0aee0"; // Public key of the wallet that allocates early contributors' bonus
